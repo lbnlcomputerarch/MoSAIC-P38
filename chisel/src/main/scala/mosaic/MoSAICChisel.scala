8914b955e5eaacc7a498d1047f87d70aa362191a
Farzad Fatollahi-Fard <ffard@lbl.gov>
2024-11-12 14:55:11 -0800

    Added DDR4 Support

--- chisel/src/main/scala/mosaic/MoSAICChisel.scala
+++ chisel/src/main/scala/mosaic/MoSAICChisel.scala
@@ -9,12 +9,16 @@ import chisel3._
 // _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
 import _root_.circt.stage.ChiselStage
 
-class MoSAICChisel extends Module {
-  val io = IO(new Bundle {
-    val control           = new AXI4LiteTargetIO()
-    val enableProcessing  = Input(Bool())
-    val internalResetDone = Output(Bool())
-  })
+class MoSAICChisel(
+  mosaicConfig: String = "mosaic_2x2_ddr4_pkt_firesim")
+    extends Module {
+  val ddr4Ctrl: Boolean = mosaicConfig.contains("ddr")
+  val io = IO(
+    new MoSAICIO(
+      ddr4Ctrl   = ddr4Ctrl,
+      ddr4Config = new DDR4Config()
+    )
+  )
 
   // Packet Interface
   val stream = Wire(new Bundle {
@@ -29,7 +33,11 @@ class MoSAICChisel extends Module {
   })
 
   // MoSAIC Black Box Instantiation
-  val tiles = Module(new mosaic())
+  val tiles = Module(
+    new mosaic(
+      mosaicConfig = mosaicConfig
+    )
+  )
 
   // Packet Interface Wiring
   // - Input
@@ -94,6 +102,55 @@ class MoSAICChisel extends Module {
 
   tiles.io.enable_processing := io.enableProcessing
   io.internalResetDone       := tiles.io.internal_rst_done
+
+  // DDR4 AXI Interface Wiring
+  if (ddr4Ctrl) {
+    tiles.io.s_axi_awready.get := io.ddr4.get.aw.ready
+    io.ddr4.get.aw.valid       := tiles.io.s_axi_awvalid.get
+    io.ddr4.get.aw.bits.id     := tiles.io.s_axi_awid.get
+    io.ddr4.get.aw.bits.addr   := tiles.io.s_axi_awaddr.get
+    io.ddr4.get.aw.bits.len    := tiles.io.s_axi_awlen.get
+    io.ddr4.get.aw.bits.size   := tiles.io.s_axi_awsize.get
+    io.ddr4.get.aw.bits.burst  := tiles.io.s_axi_awburst.get
+    io.ddr4.get.aw.bits.lock   := tiles.io.s_axi_awlock.get
+    io.ddr4.get.aw.bits.cache  := tiles.io.s_axi_awcache.get
+    io.ddr4.get.aw.bits.prot   := tiles.io.s_axi_awprot.get
+    io.ddr4.get.aw.bits.qos    := tiles.io.s_axi_awqos.get
+    // - Write
+    tiles.io.s_axi_wready.get := io.ddr4.get.w.ready
+    io.ddr4.get.w.valid       := tiles.io.s_axi_wvalid.get
+    io.ddr4.get.w.bits.last   := tiles.io.s_axi_wlast.get
+    io.ddr4.get.w.bits.data   := tiles.io.s_axi_wdata.get
+    io.ddr4.get.w.bits.strb   := tiles.io.s_axi_wstrb.get
+    // - Response Write
+    io.ddr4.get.b.ready       := tiles.io.s_axi_bready.get
+    tiles.io.s_axi_bvalid.get := io.ddr4.get.b.valid
+    tiles.io.s_axi_bid.get    := io.ddr4.get.b.bits.id
+    tiles.io.s_axi_bresp.get  := io.ddr4.get.b.bits.resp
+    // - Address Read
+    tiles.io.s_axi_arready.get := io.ddr4.get.ar.ready
+    io.ddr4.get.ar.valid       := tiles.io.s_axi_arvalid.get
+    io.ddr4.get.ar.bits.id     := tiles.io.s_axi_arid.get
+    io.ddr4.get.ar.bits.addr   := tiles.io.s_axi_araddr.get
+    io.ddr4.get.ar.bits.len    := tiles.io.s_axi_arlen.get
+    io.ddr4.get.ar.bits.size   := tiles.io.s_axi_arsize.get
+    io.ddr4.get.ar.bits.burst  := tiles.io.s_axi_arburst.get
+    io.ddr4.get.ar.bits.lock   := tiles.io.s_axi_arlock.get
+    io.ddr4.get.ar.bits.cache  := tiles.io.s_axi_arcache.get
+    io.ddr4.get.ar.bits.prot   := tiles.io.s_axi_arprot.get
+    io.ddr4.get.ar.bits.qos    := tiles.io.s_axi_arqos.get
+    // - Response Read
+    io.ddr4.get.r.ready       := tiles.io.s_axi_rready.get
+    tiles.io.s_axi_rvalid.get := io.ddr4.get.r.valid
+    tiles.io.s_axi_rlast.get  := io.ddr4.get.r.bits.last
+    tiles.io.s_axi_rdata.get  := io.ddr4.get.r.bits.data
+    tiles.io.s_axi_rid.get    := io.ddr4.get.r.bits.id
+    tiles.io.s_axi_rresp.get  := io.ddr4.get.r.bits.resp
+
+    // Clock and Reset
+    tiles.io.clk_memory.get     := clock
+    tiles.io.clk_memory_rst.get := reset.asBool
+  }
 }
 
 object Main extends App {
--- chisel/src/main/scala/mosaic/MoSAICIO.scala
+++ chisel/src/main/scala/mosaic/MoSAICIO.scala
@@ -3,6 +3,21 @@ package mosaic
 import chisel3._
 import chisel3.util._
 
+class DDR4Config(
+  val idSize:  Int = 11,
+  val adrSize: Int = 34,
+  val lenSize: Int = 8, // LENGTH
+  val szeSize: Int = 3, // SIZE
+  val brtSize: Int = 2, // BURST
+  val datSize: Int = 512, // DATA
+  val rspSize: Int = 2, // RESPONSE
+  val cacSize: Int = 4, // CACHE
+  val prtSize: Int = 3, // PROT
+  val qosSize: Int = 4 // QOS
+) {
+  val stbSize: Int = datSize / 8 // STROBE
+}
+
 class StreamIO(
   datSize: Int = 32)
     extends Bundle {
@@ -54,61 +69,187 @@ class AXI4LiteTargetIO(
 }
 
 class AXI4InitiatorIO(
-  idSize:  Int = 11,
-  adrSize: Int = 34,
-  lenSize: Int = 8, // LENGTH
-  szeSize: Int = 3, // SIZE
-  brtSize: Int = 2, // BURST
-  datSize: Int = 512, // DATA
-  rspSize: Int = 2, // RESPONSE
-  cacSize: Int = 4, // CACHE
-  prtSize: Int = 3, // PROT
-  qosSize: Int = 4 // QOS
-) extends Bundle {
-  val stbSize: Int = datSize / 8 // STROBE
+  ddr4Config: DDR4Config)
+    extends Bundle {
   // Address Write
   val aw = Decoupled(new Bundle {
-    val id    = UInt(idSize.W)
-    val addr  = UInt(adrSize.W)
-    val len   = UInt(lenSize.W)
-    val size  = UInt(szeSize.W)
-    val burst = UInt(brtSize.W)
+    val id    = UInt(ddr4Config.idSize.W)
+    val addr  = UInt(ddr4Config.adrSize.W)
+    val len   = UInt(ddr4Config.lenSize.W)
+    val size  = UInt(ddr4Config.szeSize.W)
+    val burst = UInt(ddr4Config.brtSize.W)
     val lock  = Bool()
-    val cache = UInt(cacSize.W)
-    val prot  = UInt(prtSize.W)
-    val qos   = UInt(qosSize.W)
+    val cache = UInt(ddr4Config.cacSize.W)
+    val prot  = UInt(ddr4Config.prtSize.W)
+    val qos   = UInt(ddr4Config.qosSize.W)
   })
 
   // Data Write
   val w = Decoupled(new Bundle {
     val last = Bool()
-    val data = UInt(datSize.W)
-    val strb = UInt(stbSize.W)
+    val data = UInt(ddr4Config.datSize.W)
+    val strb = UInt(ddr4Config.stbSize.W)
   })
 
   // Response Write
   val b = Flipped(Decoupled(new Bundle {
-    val id   = UInt(idSize.W)
-    val resp = UInt(rspSize.W)
+    val id   = UInt(ddr4Config.idSize.W)
+    val resp = UInt(ddr4Config.rspSize.W)
   }))
 
   // Address Read
   val ar = Decoupled(new Bundle {
-    val id    = UInt(idSize.W)
-    val addr  = UInt(adrSize.W)
-    val len   = UInt(lenSize.W)
-    val size  = UInt(szeSize.W)
-    val burst = UInt(brtSize.W)
+    val id    = UInt(ddr4Config.idSize.W)
+    val addr  = UInt(ddr4Config.adrSize.W)
+    val len   = UInt(ddr4Config.lenSize.W)
+    val size  = UInt(ddr4Config.szeSize.W)
+    val burst = UInt(ddr4Config.brtSize.W)
     val lock  = Bool()
-    val cache = UInt(cacSize.W)
-    val prot  = UInt(prtSize.W)
-    val qos   = UInt(qosSize.W)
+    val cache = UInt(ddr4Config.cacSize.W)
+    val prot  = UInt(ddr4Config.prtSize.W)
+    val qos   = UInt(ddr4Config.qosSize.W)
   })
 
   // Response Read
   val r = Flipped(Decoupled(new Bundle {
-    val data = UInt(datSize.W)
-    val id   = UInt(idSize.W)
-    val resp = UInt(rspSize.W)
+    val last = Bool()
+    val data = UInt(ddr4Config.datSize.W)
+    val id   = UInt(ddr4Config.idSize.W)
+    val resp = UInt(ddr4Config.rspSize.W)
   }))
+}
+
+class MoSAICIO(
+  bw:         Int = 32,
+  ddr4Ctrl:   Boolean,
+  ddr4Config: DDR4Config)
+    extends Bundle {
+  val control = new AXI4LiteTargetIO(
+    datSize = bw
+  )
+  val ddr4              = if (ddr4Ctrl) Some(new AXI4InitiatorIO(ddr4Config)) else None
+  val enableProcessing  = Input(Bool())
+  val internalResetDone = Output(Bool())
+}
+
+class SVMoSAICIO(
+  ddr4Ctrl:   Boolean = false,
+  bw:         Int     = 32,
+  ddr4Config: DDR4Config)
+    extends Bundle {
+  private val bwb: Int = bw / 8
+
+  //////////////////////
+  // Packet interface
+  //////////////////////
+  // Input
+  val stream_in_packet_in_TVALID = Input(Bool())
+  val stream_in_packet_in_TLAST  = Input(Bool())
+  val stream_in_packet_in_TDATA  = Input(UInt(bw.W))
+  val stream_in_packet_in_TKEEP  = Input(UInt(bwb.W))
+  val stream_in_packet_in_TREADY = Output(Bool())
+
+  // Output
+  val stream_out_packet_out_TVALID = Output(Bool())
+  val stream_out_packet_out_TLAST  = Output(Bool())
+  val stream_out_packet_out_TDATA  = Output(UInt(bw.W))
+  val stream_out_packet_out_TKEEP  = Output(UInt(bwb.W))
+  val stream_out_packet_out_TREADY = Input(Bool())
+
+  ////////////////////
+  // AXI
+  ////////////////////
+  // Address Write
+  val control_S_AXI_AWADDR  = Input(UInt(12.W))
+  val control_S_AXI_AWVALID = Input(Bool())
+  val control_S_AXI_AWREADY = Output(Bool())
+
+  // Write
+  val control_S_AXI_WDATA  = Input(UInt(bw.W))
+  val control_S_AXI_WSTRB  = Input(UInt(bwb.W))
+  val control_S_AXI_WVALID = Input(Bool())
+  val control_S_AXI_WREADY = Output(Bool())
+
+  // Write Response
+  val control_S_AXI_BRESP  = Output(UInt(2.W))
+  val control_S_AXI_BVALID = Output(Bool())
+  val control_S_AXI_BREADY = Input(Bool())
+
+  // Address Read
+  val control_S_AXI_ARADDR  = Input(UInt(12.W))
+  val control_S_AXI_ARVALID = Input(Bool())
+  val control_S_AXI_ARREADY = Output(Bool())
+
+  // Read Response
+  val control_S_AXI_RDATA  = Output(UInt(bw.W))
+  val control_S_AXI_RRESP  = Output(UInt(2.W))
+  val control_S_AXI_RVALID = Output(Bool())
+  val control_S_AXI_RREADY = Input(Bool())
+
+  ////////////////////
+  // Metadata
+  ////////////////////
+  val notify_in_metadata_in_VALID   = Input(Bool())
+  val notify_in_metadata_in_DATA    = Input(UInt(128.W))
+  val notify_out_metadata_out_VALID = Output(Bool())
+  val notify_out_metadata_out_DATA  = Output(UInt(128.W))
+
+  ////////////////////
+  // Memory Manager
+  ////////////////////
+  // - ADDRESS WRITE
+  val s_axi_awready = if (ddr4Ctrl) Some(Input(Bool())) else None
+  val s_axi_awvalid = if (ddr4Ctrl) Some(Output(Bool())) else None
+  val s_axi_awid    = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.idSize.W))) else None
+  val s_axi_awaddr  = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.adrSize.W))) else None
+  val s_axi_awlen   = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.lenSize.W))) else None
+  val s_axi_awsize  = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.szeSize.W))) else None
+  val s_axi_awburst = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.brtSize.W))) else None
+  val s_axi_awlock  = if (ddr4Ctrl) Some(Output(Bool())) else None
+  val s_axi_awcache = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.cacSize.W))) else None
+  val s_axi_awprot  = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.prtSize.W))) else None
+  val s_axi_awqos   = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.qosSize.W))) else None
+  // - DATA WRITE
+  val s_axi_wready = if (ddr4Ctrl) Some(Input(Bool())) else None
+  val s_axi_wvalid = if (ddr4Ctrl) Some(Output(Bool())) else None
+  val s_axi_wlast  = if (ddr4Ctrl) Some(Output(Bool())) else None
+  val s_axi_wdata  = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.datSize.W))) else None
+  val s_axi_wstrb  = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.stbSize.W))) else None
+  // - VALID WRITE
+  val s_axi_bready = if (ddr4Ctrl) Some(Output(Bool())) else None
+  val s_axi_bvalid = if (ddr4Ctrl) Some(Input(Bool())) else None
+  val s_axi_bid    = if (ddr4Ctrl) Some(Input(UInt(ddr4Config.idSize.W))) else None
+  val s_axi_bresp  = if (ddr4Ctrl) Some(Input(UInt(ddr4Config.rspSize.W))) else None
+  // - ADDRESS READ
+  val s_axi_arready = if (ddr4Ctrl) Some(Input(Bool())) else None
+  val s_axi_arvalid = if (ddr4Ctrl) Some(Output(Bool())) else None
+  val s_axi_arid    = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.idSize.W))) else None
+  val s_axi_araddr  = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.adrSize.W))) else None
+  val s_axi_arlen   = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.lenSize.W))) else None
+  val s_axi_arsize  = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.szeSize.W))) else None
+  val s_axi_arburst = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.brtSize.W))) else None
+  val s_axi_arlock  = if (ddr4Ctrl) Some(Output(Bool())) else None
+  val s_axi_arcache = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.cacSize.W))) else None
+  val s_axi_arprot  = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.prtSize.W))) else None
+  val s_axi_arqos   = if (ddr4Ctrl) Some(Output(UInt(ddr4Config.qosSize.W))) else None
+  // - RESPONSE READ
+  val s_axi_rready = if (ddr4Ctrl) Some(Output(Bool())) else None
+  val s_axi_rvalid = if (ddr4Ctrl) Some(Input(Bool())) else None
+  val s_axi_rlast  = if (ddr4Ctrl) Some(Input(Bool())) else None
+  val s_axi_rdata  = if (ddr4Ctrl) Some(Input(UInt(ddr4Config.datSize.W))) else None
+  val s_axi_rid    = if (ddr4Ctrl) Some(Input(UInt(ddr4Config.idSize.W))) else None
+  val s_axi_rresp  = if (ddr4Ctrl) Some(Input(UInt(ddr4Config.rspSize.W))) else None
+
+  val clk_memory     = if (ddr4Ctrl) Some(Input(Clock())) else None
+  val clk_memory_rst = if (ddr4Ctrl) Some(Input(Reset())) else None
+
+  ////////////////////
+  // Clock and Reset
+  ////////////////////
+  val clk_line_rst      = Input(Reset())
+  val clk_line          = Input(Clock())
+  val clk_control_rst   = Input(Reset())
+  val clk_control       = Input(Clock())
+  val enable_processing = Input(Bool())
+  val internal_rst_done = Output(Bool())
 }
